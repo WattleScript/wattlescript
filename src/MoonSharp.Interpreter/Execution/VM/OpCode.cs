048c74870397a5473efec3fe774726667425e4fe
lofcz <stagl.matej@centrum.cz>
2022-04-04 06:37:34 +0200

    Reorder

--- src/MoonSharp.Interpreter/Execution/VM/OpCode.cs
+++ src/MoonSharp.Interpreter/Execution/VM/OpCode.cs
@@ -79,9 +79,10 @@ namespace MoonSharp.Interpreter.Execution.VM
 		// Iterators
 		IterPrep,   // Prepares an iterator for execution 
 		IterUpd,	// Updates the var part of an iterator
+		
+		NilCoalesing,
 
 		// Meta
 		Invalid,	// Crashes the executor with an unrecoverable NotImplementedException. This MUST always be the last opcode in enum
-		NilCoalesing
 	}
 }
