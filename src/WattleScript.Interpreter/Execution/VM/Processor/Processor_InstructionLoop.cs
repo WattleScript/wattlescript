WattleScript/wattlescript

commit 298d83abc69856865c9fbd6b2640f4d686f5b438

Basic table prototype

src/WattleScript.Interpreter/Execution/VM/Processor/Processor_InstructionLoop.cs
@@ -1705,7 +1705,22 @@ namespace WattleScript.Interpreter.Execution.VM
 						{
 							if (isMultiIndex) throw new ScriptRuntimeException("cannot multi-index a table. userdata expected");
 
-							m_ValueStack.Push(DynValue.Nil);
+							//Check prototype for method call syntax
+							if (i.NumVal2 != 0)
+							{
+								var tb = m_Script.GetTablePrototype();
+								if (tb != null)
+								{
+									var v = tb.Get(idx);
+									v.FromMetatable = true;
+									m_ValueStack.Push(v);
+								} 
+							}
+							else
+							{
+								//Not a wattle method call, don't check prototype
+								m_ValueStack.Push(DynValue.Nil);
+							}
 							return instructionPtr;
 						}
 

src/WattleScript.Interpreter/CoreLib/PrototypeModule.cs
@@ -16,6 +16,7 @@ namespace WattleScript.Interpreter.CoreLib
             proto.Set("boolean", sc.Registry.Get(BOOLEAN_PROTOTABLE));
             proto.Set("range", sc.Registry.Get(RANGE_PROTOTABLE));
             proto.Set("string", DynValue.NewTable(stringTable));
+            proto.Set("table", DynValue.NewTable(sc.GetTablePrototype()));
         }
 
         public static void EnablePrototypes(Table globalTable)
@@ -55,6 +56,8 @@ namespace WattleScript.Interpreter.CoreLib
                 //functions
                 rFuncs.Set("tostring", DynValue.NewCallback(BasicModule.tostring, "tostring"));
             }
+            if(sc.GetTablePrototype() == null)
+                sc.SetTablePrototype(new Table(sc));
             //Copy tostring to string table
             stringTable.Set("tostring", DynValue.NewCallback(BasicModule.tostring, "tostring"));
         }

src/WattleScript.Interpreter/Script.cs
@@ -71,6 +71,7 @@ namespace WattleScript.Interpreter
 		Table m_GlobalTable;
 		IDebugger m_Debugger;
 		Table[] m_TypeMetatables = new Table[(int)LuaTypeExtensions.MaxMetaTypes];
+		Table m_TablePrototype;
 		internal List<ScriptParserMessage> i_ParserMessages { get; set; } = new List<ScriptParserMessage>();
 
 		/// <summary>
@@ -901,7 +902,7 @@ namespace WattleScript.Interpreter
 		/// <summary>
 		/// Sets a type metatable.
 		/// </summary>
-		/// <param name="type">The type. Must be Nil, Boolean, Number, String or Function</param>
+		/// <param name="type">The type. Must be Nil, Boolean, Number, String, Range or Function</param>
 		/// <param name="metatable">The metatable.</param>
 		/// <exception cref="System.ArgumentException">Specified type not supported :  + type.ToString()</exception>
 		public void SetTypeMetatable(DataType type, Table metatable)
@@ -915,6 +916,17 @@ namespace WattleScript.Interpreter
 			else
 				throw new ArgumentException("Specified type not supported : " + type.ToString());
 		}
+
+		/// <summary>
+		/// Sets the prototype for tables
+		/// </summary>
+		internal void SetTablePrototype(Table prototype)
+		{
+			this.CheckScriptOwnership(prototype);
+			m_TablePrototype = prototype;
+		}
+
+		internal Table GetTablePrototype() => m_TablePrototype;
 
 
 		/// <summary>

src/WattleScript.Tests/EndToEnd/CLike/SyntaxCLike/Meta/3-table.lua
@@ -1,0 +1,10 @@
+function prototype.table.hello()
+{
+    return "hello";
+}
+
+tbl = {}
+print(tbl.hello());
+//special case for tables, we only index the prototype for direct method calls
+//this should be nil
+print(tbl.hello)

commit df4043a9a962a8da0605e3f64c01ca24e55238f3

Fix test failures introduced in 298d83a

src/WattleScript.Interpreter/Execution/VM/Processor/Processor_InstructionLoop.cs
@@ -1714,7 +1714,12 @@ namespace WattleScript.Interpreter.Execution.VM
 									var v = tb.Get(idx);
 									v.FromMetatable = true;
 									m_ValueStack.Push(v);
-								} 
+								}
+								else
+								{
+									//No prototype, nil
+									m_ValueStack.Push(DynValue.Nil);
+								}
 							}
 							else
 							{

src/WattleScript.Interpreter/Tree/Expressions/FunctionCallExpression.cs
@@ -22,6 +22,8 @@ namespace WattleScript.Interpreter.Tree.Expressions
 		string m_DebugErr;
 		private CallKind m_Kind;
 		internal SourceRef SourceRef { get; private set; }
+		
+		private bool wattleCallSyntax;
 
 		public FunctionCallExpression(ScriptLoadingContext lcontext, Expression function, Token thisCallName, CallKind kind)
 			: base(lcontext)
@@ -32,6 +34,7 @@ namespace WattleScript.Interpreter.Tree.Expressions
 			m_DebugErr = function.GetFriendlyDebugName();
 			m_Function = function;
 			m_Kind = kind;
+			wattleCallSyntax = lcontext.Syntax == ScriptSyntax.WattleScript;
 			
 			switch (lcontext.Lexer.Current.Type)
 			{
@@ -88,6 +91,7 @@ namespace WattleScript.Interpreter.Tree.Expressions
 			bc.PushSourceRef(SourceRef);
 			int argslen = m_Arguments.Count;
 
+			bool isMethodCall = wattleCallSyntax && m_Kind != CallKind.Normal;
 			if (m_Kind == CallKind.ImplicitThisCall && m_Name == null)
 			{
 				((IndexExpression)m_Function).Compile(bc, true, true);
@@ -108,7 +112,7 @@ namespace WattleScript.Interpreter.Tree.Expressions
 			if (!string.IsNullOrEmpty(m_Name))
 			{
 				bc.Emit_Copy(0);
-				bc.Emit_Index(m_Name, true, isMethodCall: m_Kind != CallKind.Normal);
+				bc.Emit_Index(m_Name, true, isMethodCall: isMethodCall);
 				bc.Emit_Swap(0, 1);
 				++argslen;
 			}